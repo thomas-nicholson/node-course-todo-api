feea08ecbb192866b51b11abf8f15545d167cc78
isthatagrapefrut <wkerr1996@gmail.com>
2018-08-16 10:56:03 +1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    console.log('Id not valid');
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+
+  }).catch((e) => res.status(400).send());
+
 });
 
 app.listen(3000, () => {
